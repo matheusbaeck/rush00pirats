cc728b02c35dee6f01f5ddb889229083843cff1a
mamagalh <matheusbaeck@gmail.com>
2022-11-05 10:12:58 +0100

    "git creation file"

---
+++ creat_repo.sh
@@ -0,0 +1,7 @@
+echo "# rush00pirats" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/matheusbaeck/rush00pirats.git
+git push -u origin main
